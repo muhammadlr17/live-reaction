dfb022f5e10d8f0b1b2d4511b8c05986eeae79b7
Muhammad Lutfi Ramadhan <muhammadlr17@gmail.com>
2026-08-21 16:32:44 +0700

    Add:
    Add dynamic data and modify view

---
+++ data.js
@@ -0,0 +1,18 @@
+const dataWisudawan = [
+  {
+    "namaLengkap": "Muhammad Lutfi Ramadhan, A.Md.Kom.",
+    "nrp": "12345678",
+    "prodi": "D3 Teknik Informatika",
+    "jurusan": "Jurusan Teknologi Elektro",
+    "predikat": "Dengan Pujian",
+    "url_foto": "https://placehold.co/300x400/3498db/ffffff?text=FOTO+LUTFI"
+  },
+  {
+    "namaLengkap": "Budi Susanto, A.Md.T.",
+    "nrp": "12345679",
+    "prodi": "D3 Teknik Listrik",
+    "jurusan": "Jurusan Teknologi Elektro",
+    "predikat": "Sangat Memuaskan",
+    "url_foto": "https://placehold.co/300x400/e74c3c/ffffff?text=FOTO+BUDI"
+  }
+];
